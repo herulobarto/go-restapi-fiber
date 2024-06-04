32247f26f962fb92abb42700a03fc55240bec9ab
herulobarto <herulobarto@gmail.com>
2024-06-04 11:42:45 +0700

    membuat func create untuk menambah data

--- controllers/bookcontroller/bookcontroller.go
+++ controllers/bookcontroller/bookcontroller.go
@@ -19,7 +19,21 @@ func Show(c *fiber.Ctx) error {
 }
 
 func Create(c *fiber.Ctx) error {
-	return nil
+
+	var book models.Book
+	if err := c.BodyParser(&book); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	if err := models.DB.Create(&book).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	return c.JSON(book)
 
 }
 
