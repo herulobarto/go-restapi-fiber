87a3b84629662191101f4fd4c1bbc345e3c648a1
herulobarto <herulobarto@gmail.com>
2024-06-04 11:18:45 +0700

    membuat koneksi dan folder yang diperlukan

---
+++ controllers/bookcontroller/bookcontroller.go
@@ -0,0 +1,27 @@
+package bookcontroller
+
+import "github.com/gofiber/fiber/v2"
+
+func Index(c *fiber.Ctx) error {
+	return nil
+}
+
+func Show(c *fiber.Ctx) error {
+	return nil
+
+}
+
+func Create(c *fiber.Ctx) error {
+	return nil
+
+}
+
+func Update(c *fiber.Ctx) error {
+	return nil
+
+}
+
+func Delete(c *fiber.Ctx) error {
+	return nil
+
+}
